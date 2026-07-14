216bbbda8c12bc359efd0201ad3cab10790d68a3
Adarsh Parvathaneni <s87pkwjs2p@privaterelay.appleid.com>
2026-07-14 15:28:20 +0530

    Add files via upload

---
+++ BasicJavaCommands.java
@@ -0,0 +1,17 @@
+public class BasicJavaCommands {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+        int number = 10;
+        double decimal = 5.5;
+        String text = "Java Programming";
+
+        int sum = number + 5;
+        double product = decimal * 2;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+        System.out.println("Text: " + text);
+    }   
+}
